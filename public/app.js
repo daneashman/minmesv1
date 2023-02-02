c5424598513c2594f3863ca1dace1e4e2cce244a
daneashman <daneashman@outlook.com>
2023-02-02 13:18:02 -0800

    fixed recaptcha

--- public/app.js
+++ public/app.js
@@ -3,7 +3,14 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 ui.start('#firebaseui-auth-container', {
     signInOptions: [
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
+      {
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        recaptchaParameters: {
+          type: '', // 'audio'
+          size: 'invisible', // 'invisible' or 'compact'
+          badge: 'bottomleft' //' bottomright' or 'inline' applies to invisible.
+        }
+      }
+    ]
     // Other config options...
   });
